6ad8e7ead136c28cd1b6798c151886a3182f98fd
sfc-gh-naveenalanthomas <99874664+naveenalanthomas@users.noreply.github.com>
2024-04-19 11:09:06 +0100

    Update Home.py

--- Snowflake_for_Marketing_App/streamlit/Home.py
+++ Snowflake_for_Marketing_App/streamlit/Home.py
@@ -21,7 +21,7 @@ with col1:
    compaign_content_b64encoded = base64.b64encode(compaign_content_bytes).decode()
    compaign_image_string = f'data:image/{campaign_mime_type};base64,{compaign_content_b64encoded}'
    st.image(compaign_image_string)
-   st.write("Click to open the [app](https://app.snowflake.com/sfsenorthamerica/polaris1/#/streamlit-apps/CUSTOMER72_DB.DEMO.CAMPAIGN_72_APP)")
+   
 with col2:
    st.subheader(':orange[Customer 360]')
    image_name = 'images/Customer_Starter.png'
@@ -31,4 +31,4 @@ with col2:
    content_b64encoded = base64.b64encode(content_bytes).decode()
    image_string = f'data:image/{mime_type};base64,{content_b64encoded}'
    st.image(image_string)
-   st.write("Click to open the [app](https://app.snowflake.com/sfsenorthamerica/polaris1/#/streamlit-apps/CUSTOMER72_DB.DEMO.CAMPAIGN_72_APP)")
+   
